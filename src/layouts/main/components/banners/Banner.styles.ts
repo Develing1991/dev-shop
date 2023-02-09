3a8fa6408156e4b7cab90aef3fd0d2d2a5e9de9b
Develing1991 <completed0728@gmail.com>
2023-02-10 08:16:05 +0900

    배너 반응형

--- src/layouts/main/components/banners/Banner.styles.ts
+++ src/layouts/main/components/banners/Banner.styles.ts
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
 import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
+import SlickSlider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Banner = styled.section`
   .inner {
@@ -7,17 +10,81 @@ export const Banner = styled.section`
     margin: auto;
     position: relative;
   }
-  .wrapper {
+  .btn-wrapper {
     max-width: 1024px;
     margin: auto;
-    left: 0;
     right: 0;
-    top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     margin: auto;
     position: absolute;
+  }
+  &:hover .prev-arrow,
+  &:hover .next-arrow {
+    opacity: 0.3;
+    pointer-events: auto;
+  }
+`;
+export const Slider = styled(SlickSlider)`
+  .slick-slide div {
+    outline: none;
+  }
+
+  .slick-dots {
+    position: absolute;
+    bottom: 5px;
+    display: flex !important; /* 인라인으로 되어있어서 강제로.. */
+    justify-content: center;
+    gap: 5px;
+  }
+  .slick-dots li {
+    margin: 0;
+    width: 10px;
+    height: 10px;
+  }
+  .slick-dots li button {
+    padding: 0;
+    width: 10px;
+    height: 10px;
+  }
+
+  .slick-dots li button:before {
+    width: 10px;
+    height: 10px;
+    opacity: 1;
+    color: var(--color-white);
+    background-color: transparent;
+    line-height: 1;
+  }
+  .slick-dots li.slick-active button:before {
+    opacity: 1;
+    color: var(--color-pink);
+  }
+
+  @media screen and (max-width: 1024px) {
+    .slick-list {
+      width: 110%;
+      margin-left: -5%;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    .slick-list {
+      width: 130%;
+      margin-left: -15%;
+    }
+  }
+  @media screen and (max-width: 640px) {
+    .slick-list {
+      width: 140%;
+      margin-left: -18%;
+    }
+  }
+  @media screen and (max-width: 480px) {
+    .slick-list {
+      width: 150%;
+      margin-left: -22%;
+    }
   }
 `;
 
@@ -26,24 +93,35 @@ export const PrevArrow = styled(AiFillLeftCircle)`
   top: 0;
   bottom: 0;
   margin: auto;
-  left: 0;
+  left: 15px;
   width: 50px;
   height: 50px;
   color: black;
-  opacity: 0.3;
   z-index: 1;
   cursor: pointer;
+  pointer-events: none;
+  opacity: 0;
+  transition: all 0.2s ease-in;
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
 `;
+
 export const NextArrow = styled(AiFillRightCircle)`
   position: absolute;
   top: 0;
   bottom: 0;
   margin: auto;
-  right: 0;
+  right: 15px;
   width: 50px;
   height: 50px;
   color: black;
-  opacity: 0.3;
   z-index: 1;
   cursor: pointer;
+  pointer-events: none;
+  opacity: 0;
+  transition: all 0.2s ease-in;
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
 `;
--- src/layouts/main/components/banners/Banner.tsx
+++ src/layouts/main/components/banners/Banner.tsx
@@ -1,33 +1,43 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-
 import * as S from "./Banner.styles";
 export default function Banner() {
   const settings = {
     dots: false,
-    infinite: false,
-    speed: 500,
+    infinite: true,
+    speed: 800,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          dots: true,
+        },
+      },
+    ],
   };
+
   return (
     <S.Banner>
       <div className="inner">
-        <div className="wrapper">
-          <S.PrevArrow />
-          <S.NextArrow />
+        <div className="btn-wrapper">
+          <S.PrevArrow className="prev-arrow" />
+          <S.NextArrow className="next-arrow" />
         </div>
-        <Slider {...settings}>
-          {[1, 2, 3, 4, 5].map((el) => (
-            <div key={el} className="image-area">
-              <img
-                src="http://responsive-sample42.firstmall.kr/data/skin/responsive_babygoods_cute_gl/images/banner/11/images_2.jpg"
-                alt="slick1"
-              />
-            </div>
-          ))}
-        </Slider>
+        <div>
+          <S.Slider {...settings}>
+            {[1, 2, 3, 4, 5].map((el) => (
+              <div key={el} className="image-area">
+                <img
+                  src="http://responsive-sample42.firstmall.kr/data/skin/responsive_babygoods_cute_gl/images/banner/11/images_2.jpg"
+                  alt="slick1"
+                  className="slick-image"
+                />
+              </div>
+            ))}
+          </S.Slider>
+        </div>
       </div>
     </S.Banner>
   );
