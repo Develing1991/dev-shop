868e5d9b14bd673ec2cdc979c2581056ee1ead38
Develing1991 <completed0728@gmail.com>
2023-02-10 11:32:12 +0900

    추천상품 반응형

--- src/layouts/main/components/recommend/RecommendProducts.styles.ts
+++ src/layouts/main/components/recommend/RecommendProducts.styles.ts
@@ -4,4 +4,13 @@ export const RecommendProducts = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
