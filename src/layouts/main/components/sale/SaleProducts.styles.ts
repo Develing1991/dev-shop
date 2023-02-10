893e69f1f01da0aeb0b7aa889b33a22effd9bde6
Develing1991 <completed0728@gmail.com>
2023-02-10 11:13:57 +0900

    세일 상품 반응형

--- src/layouts/main/components/sale/SaleProducts.styles.ts
+++ src/layouts/main/components/sale/SaleProducts.styles.ts
@@ -2,12 +2,18 @@ import styled from "@emotion/styled";
 
 export const SaleProducts = styled.div`
   ul {
-    display: flex;
-    align-items: flex-start;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
     gap: 15px;
-  }
-  ul li:first-child {
-    flex-basis: 30%;
+    @media screen and (max-width: 1024px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (max-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: 480px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
   }
   .title-group {
     margin-top: 0;
