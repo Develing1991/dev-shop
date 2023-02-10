3fa4a5809f78843d3542a7b56e3bcd2c31ca2a91
Develing1991 <completed0728@gmail.com>
2023-02-10 15:53:55 +0900

    terms 반응형

--- src/layouts/footers/components/Terms/Terms.styles.ts
+++ src/layouts/footers/components/Terms/Terms.styles.ts
@@ -4,9 +4,18 @@ export const Terms = styled.div`
   font-size: var(--text-xs);
   font-weight: var(--font-regular);
   margin-top: 30px;
+  @media screen and (max-width: 640px) {
+    margin-top: 0px;
+    border-bottom: 1px dotted var(--color-gray-1);
+    padding-bottom: 20px;
+    margin-bottom: 15px;
+  }
   ul {
     display: flex;
     gap: 20px;
+    @media screen and (max-width: 640px) {
+      flex-direction: column;
+    }
   }
 
   li a {
