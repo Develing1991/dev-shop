22ee8a4ba62c3201eb5cfc1e0998d14dd43a19ca
Develing1991 <completed0728@gmail.com>
2023-02-11 03:26:38 +0900

    회원가입 tabindex 추가

--- pages/members/signin/index.tsx
+++ pages/members/signin/index.tsx
@@ -5,37 +5,55 @@ export default function SignInPage() {
       <h2 className="title">LOGIN</h2>
       <form>
         <label htmlFor="email" />
-        <input type="email" id="email" className="email" placeholder="이메일" />
+        <input
+          type="email"
+          id="email"
+          className="email"
+          placeholder="이메일"
+          tabIndex={1}
+        />
         <label htmlFor="password" />
         <input
           type="password"
           id="password"
           className="password"
           placeholder="비밀번호"
+          tabIndex={2}
         />
         <div className="save">
-          <input type="checkbox" id="saveId" className="checkbox" />
+          <input
+            type="checkbox"
+            id="saveId"
+            className="checkbox"
+            tabIndex={3}
+          />
           <label htmlFor="saveId">아이디 저장</label>
         </div>
-        <button type="submit" className="signin-btn">
+        <button type="submit" className="signin-btn" tabIndex={4}>
           로그인
         </button>
       </form>
       <ul className="breadcrumbs">
         <li>
-          <a href="#">아이디&nbsp;찾기</a>
+          <a href="#" tabIndex={4}>
+            아이디&nbsp;찾기
+          </a>
         </li>
         <li>
-          <a href="#">비밀번호&nbsp;찾기</a>
+          <a href="#" tabIndex={5}>
+            비밀번호&nbsp;찾기
+          </a>
         </li>
         <li>
-          <a href="#">회원가입</a>
+          <a href="#" tabIndex={6}>
+            회원가입
+          </a>
         </li>
       </ul>
       <div className="others">
-        <S.GoogleIcon />
-        <S.FacebookIcon />
-        <S.TwitterIcon />
+        <S.GoogleIcon tabIndex={7} />
+        <S.FacebookIcon tabIndex={8} />
+        <S.TwitterIcon tabIndex={9} />
       </div>
     </S.SignInPage>
   );
