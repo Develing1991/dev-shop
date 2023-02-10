c476d366f087f47ba302086d789c86fa74c0c3b9
Develing1991 <completed0728@gmail.com>
2023-02-10 11:02:22 +0900

    AddBanner 반응형

---
+++ src/layouts/main/components/banners/AdBanner.styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const AddBanner = styled.div`
+  @media screen and (max-width: 480px) {
+    img {
+      display: none;
+    }
+  }
+`;
--- src/layouts/main/components/banners/AdBanner.tsx
+++ src/layouts/main/components/banners/AdBanner.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+import * as S from "./AdBanner.styles";
 export default function AdBanner() {
   return (
-    <div>
+    <S.AddBanner>
       <a href="">
         <img
           src="http://responsive-sample42.firstmall.kr/data/skin/responsive_babygoods_cute_gl/images/design_resp/thumb2_1.jpg"
           alt="ad Banner"
         />
       </a>
-    </div>
+    </S.AddBanner>
   );
 }
