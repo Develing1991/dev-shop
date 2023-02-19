c5cc86a7fad4723fdc7bc1ddd131e2acf9363486
Develing1991 <completed0728@gmail.com>
2023-02-19 23:31:33 +0900

    정적,동적 빌드시 exportPathMap 설정.. entries로 확인해서 동적파일 제외

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "suhan_lee_deploy_id",
+  // // 정적 파일 빌드 직접 지정
+  // exportPathMap: () => ({
+  //   "/": { page: "/" },
+  //   "/boards": { page: "/boards" },
+  //   "/404": { page: "/404" },
+  // }),
+  // 정적파일 로지컬
+  exportPathMap: function (defaultPathMap) {
+    let staticPathMap = {};
+    for (const [key, value] of Object.entries(defaultPathMap)) {
+      if (!key.includes("[") && !key.includes("]")) {
+        staticPathMap = {
+          ...staticPathMap,
+          [key]: value,
+        };
+      }
+    }
+    console.log(staticPathMap);
+
+    return staticPathMap;
+  },
 };
 
 module.exports = nextConfig;
